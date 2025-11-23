4258bceaf41cd1d38af96567df7b4e5887898c0a
Prof-rajendra <Prof.rajendrakumarshah@gmail.com>
2025-11-23 23:34:10 +0000

    added delete article route

--- router/router.js
+++ router/router.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { login, createArticle, getAllArticles, getArticleById, createAdmin } = require('../controllers/admin');
+const { login, createArticle, getAllArticles, getArticleById, createAdmin, deleteArticleById } = require('../controllers/admin');
 const authMiddleware = require('../middleware/middleware');
 
-router.post('/articles', createArticle );
+router.post('/articles', authMiddleware, createArticle );
 router.get('/articles', getAllArticles );
 router.get('/articles/:id', getArticleById );
 router.post('/admin', createAdmin);
+router.delete('/articles/:id', authMiddleware, deleteArticleById );
 router.post('/login', login );
 
 router.get('/', (req, res) => {
